3b459827ef07bfa23284281d1ef62eaa3a780082
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-06-04 10:47:21 -0700

    jel add self.id to basemodel class

---
+++ models/base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""BaseModel class module that all AirBnB classes will inherit from"""
+
+
+import json
+import uuid
+
+
+class BaseModel:
+    """Defines all common attributes & methods for AirBnB classes"""
+    def __init__(self, id, created_at, updated_at):
+        self.id =  str(uuid.uuid4())
+        
