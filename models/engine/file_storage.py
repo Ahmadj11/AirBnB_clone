1faf73f3f939aa814d77629ed80cdff49d4c91b3
iamjessicaelaine <95127791+iamjessicaelaine@users.noreply.github.com>
2022-06-13 08:13:19 -0500

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,6 +40,7 @@ class FileStorage:
                 redfile = jfile.read()
                 # load read json file into  __objects
                 tempobjects = json.loads(redfile)
+            # create dictionary within dictionary
             for key, value in tempobjects.items():
                 self.__objects[key] = value.to_dict() 
         else:
